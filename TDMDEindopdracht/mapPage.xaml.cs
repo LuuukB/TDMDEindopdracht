3160ba09d02c6e7178ca490c04f229a0a483f5f4
Zehond <fsas.7seal@gmail.com>
2024-12-19 16:34:07 +0100

    map inladen werkt. en .gitgnore aangepast

--- TDMDEindopdracht/mapPage.xaml.cs
+++ TDMDEindopdracht/mapPage.xaml.cs
@@ -1,9 +1,11 @@
 namespace TDMDEindopdracht;
-
+using  Map = Microsoft.Maui.Controls.Maps.Map;
 public partial class mapPage : ContentPage
 {
 	public mapPage()
 	{
 		InitializeComponent();
+		Map map = new Map();
+		Content = map;
 	}
 }
