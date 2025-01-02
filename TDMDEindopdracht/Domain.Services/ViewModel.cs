713305a94e39fa229c33ab5cb29b210b8a44f2f1
Luuk <luukboerman@hotmail.com>
2025-01-02 15:39:09 +0100

    naam property toegevoegd aan viewmodel zodat we wss die kunnen opvragen voor de routes. andere weet ik neit meer xD

--- TDMDEindopdracht/Domain.Services/ViewModel.cs
+++ TDMDEindopdracht/Domain.Services/ViewModel.cs
@@ -1,22 +1,42 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDMDEindopdracht.Domain.Model;
 
 namespace TDMDEindopdracht.Domain.Services
 {
     public partial class ViewModel : ObservableObject
     {
         IGeolocation geolocation;
-        public ViewModel(IGeolocation geolocation)
+        
+        private IDatabaseCommunicator _communicator;
+        public ViewModel(IGeolocation geolocation, IDatabaseCommunicator communicator)
         {
             this.geolocation = geolocation;
-
+            _communicator = communicator;
+            AddRoutes();
         }
-
+        [ObservableProperty]private ObservableCollection<Route> _routes = [];
+        [ObservableProperty] private string _name;
+        [ObservableProperty] private Route _selectedRoute;
+        [ObservableProperty] private string _selectedName;
+        [ObservableProperty] private string _selectedDistance;
+        [ObservableProperty] private string _selectedTotalRunTime;
+        [ObservableProperty] private string _selectedAveradgeSpeed;
+        partial void OnSelectedRouteChanged(Route value)
+        {
+            SelectedName = value?.Name ?? string.Empty;
+            SelectedDistance = value?.Distance.ToString() ?? string.Empty;
+            SelectedTotalRunTime = value?.TotalRunTime ?? string.Empty;
+            SelectedAveradgeSpeed = value?.AveradgeSpeed.ToString() ?? string.Empty;
+        }
 
         [RelayCommand]
         public async Task GetCurrentLocation()
@@ -28,6 +48,16 @@ namespace TDMDEindopdracht.Domain.Services
         private async Task ViewMap()
         {
             await Shell.Current.GoToAsync("//mapPage");
+        }
+
+        public void AddRoutes() { 
+            var pastRoutes = _communicator.GetAllRoutes();
+            Routes.Clear();
+
+            foreach (var route in pastRoutes)
+            {
+                Routes.Add(route);
+            }
         }
 
     }
